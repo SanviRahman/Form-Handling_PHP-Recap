256839e157b49b26c6ba5f51152d06913e22b250
Sheikh Forid Ahmed Shanto <Sheikh15-3700@diu.edu.bd>
2025-09-22 12:09:07 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $nameErr = $emailErr = $websiteErr = $genderErr = " ";
+    ?>
+    <form action="welcome.php" method="post">
+        <label for="name">Name:</label>
+        <input type="name" name="name" id="email" placeholder="Enter your name..." required>
+
+        <pre></pre>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" placeholder="Enter your email..." required>
+
+        <pre></pre>
+        <label for="Website">Website</label>
+        <input type="website" name="website" id="website" placeholder="Enter your website" required>
+
+        <pre></pre>
+        comment: <textarea name="comment" rows="5" cols="40"></textarea>
+
+        <pre></pre>
+        <label for="gender">Gender:</label>
+        <input type="radio" name="gender" value="Female" required>Female
+        <input type="radio" name="gender" value="Male" required>Male
+        <input type="radio" name="gender" value="Others" required>Others
+
+        <pre></pre>
+        <button type="submit">Submit</button>
+    </form>
+
+
+</body>
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $name = $email = $gender = $comment = $website =  "";
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data, ENT_QUOTES, "UTF-8");
+
+        return $data;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is Required.";
+        } else {
+            $name = test_input($_POST["name"]);
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is Requird.";
+        } else {
+            $email = test_input($_POST["email"]);
+        }
+        if (empty($_POST["comment"])) {
+            $comment = "";
+        } else {
+            $comment = test_input($_POST["comment"]);
+        }
+        if (empty($_POST["website"])) {
+            $websiteErr = "Website is Required.";
+        } else {
+            $website = test_input($_POST["website"]);
+        }
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+        } else {
+            $gender = test_input($_POST["gender"]);
+        }
+
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $nameErr == "" && $emailErr == "" && $genderErr == "") {
+            echo "<div class='output-box'>";
+            echo "<h2>Your Output:</h2>";
+            echo "<p><strong>Name:</strong> " . $name . "</p>";
+            echo "<p><strong>Email:</strong> " . $email . "</p>";
+            echo "<p><strong>Website:</strong> " . $website . "</p>";
+            echo "<p><strong>Comment:</strong> " . $comment . "</p>";
+            echo "<p><strong>Gender:</strong> " . $gender . "</p>";
+            echo "</div>";
+        }
+    }
+    ?>
+</body>
+
+</html>
